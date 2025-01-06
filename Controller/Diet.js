49e97f683a08a72838d1fa89a4dd55cebfb64920
Karim Adel <121738162+karimAdel2002@users.noreply.github.com>
2025-01-06 19:20:05 +0200

    Update Diet.js

--- Controller/Diet.js
+++ Controller/Diet.js
@@ -3,7 +3,7 @@ import Photo_Gallaries from "../DB Models/Photo_Gallary.js"
 export const index = async (req, res) => {
     try{
         const  Photo_Gallary= await Photo_Gallaries.find({}).lean();
-    res.render('Pages/Read_more',{Photo_Gallary})
+    res.render('Pages/Diet',{Photo_Gallary})
 } catch (error) {
     console.error(error);
     res.status(500).render("Pages/404", { error });
